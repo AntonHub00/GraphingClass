e73227c9c07710eea6bfae0b265f68ed8a2c52e8
Antonio Ochoa <antonhub00@gmail.com>
2018-09-02 00:25:26 -0500

    Issues fixed
    The mushroom is centered.
    The color inside of the mushroom is set (done).
    The matrix is still 40*40.

--- test_draw.c
+++ test_draw.c
@@ -20,32 +20,40 @@ int main()
     set_background(image, 0);
 
     //Left side
-    paint(image, 16, 4, 30, 6, 1);
+    paint(image, 14, 4, 28, 6, 1);
 
     //Up-left side
-    paint(image, 12, 6, 18, 8, 1);
-    paint(image, 10, 8, 14, 10, 1);
-    paint(image, 8, 10, 12, 12, 1);
-    paint(image, 8, 12, 10, 16, 1);
+    paint(image, 10, 6, 16, 8, 1);
+    paint(image, 8, 8, 12, 10, 1);
+    paint(image, 6, 10, 10, 12, 1);
+    paint(image, 6, 12, 8, 16, 1);
 
     //top
-    paint(image, 6, 14, 8, 20, 1);
+    paint(image, 4, 14, 6, 20, 1);
 
     //Down-left side
-    paint(image, 28, 6, 32, 8, 1);
-    paint(image, 28, 8, 36, 10, 1);
-    paint(image, 34, 10, 38, 12, 1);
+    paint(image, 26, 6, 30, 8, 1);
+    paint(image, 26, 8, 34, 10, 1);
+    paint(image, 32, 10, 36, 12, 1);
 
     //Down
-    paint(image, 36, 12, 38, 20, 1);
+    paint(image, 34, 12, 36, 20, 1);
 
     //Face
-    paint(image, 26, 10, 30, 12, 1);
-    paint(image, 26, 12, 28, 20, 1);
-    paint(image, 28, 16, 32, 18, 1);
+    paint(image, 24, 10, 28, 12, 1);
+    paint(image, 24, 12, 26, 20, 1);
+    paint(image, 26, 16, 30, 18, 1);
 
-    //Inside mushroom
-    // paint(image, 14, 6, 20, 12, 5);
+    //Inside mushroom (5 up)
+    paint(image, 10, 10, 14, 12, 7);
+    paint(image, 12, 12, 22, 14, 7);
+    paint(image, 12, 14, 16, 16, 7);
+    paint(image, 10, 16, 14, 18, 7);
+    paint(image, 6, 18, 14, 20, 1);
+
+    //Inside mushroom (2 down)
+    paint(image, 22, 6, 24, 16, 7);
+    paint(image, 24, 6, 26, 10, 7);
 
     //Completing the half "automatically"
     complete_half(image);
