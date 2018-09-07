2aa8f185c450654156580138a75e643b74ba9eea
AntonHub <antonhub00@gmail.com>
2018-09-07 14:44:11 -0500

    Added fibonacci.c file
    It fills a matrix with the fibonacci numbers starting i the center
    and continues in spiral until it gets full.
    Bug in filling.
    Almost done.

---
+++ fibonacci.c
@@ -0,0 +1,146 @@
+/*
+Fibonacci display in spiral
+*/
+#include <stdio.h>
+
+#define SIZE 3
+
+void fibonacci(int ptr_matrix[][SIZE]);
+void print_matrix(int ptr_matrix[][SIZE]);
+
+int main()
+{
+    int matrix[SIZE][SIZE] = {0};
+
+    fibonacci(matrix);
+
+    print_matrix(matrix);
+
+    return 0;
+}
+
+void fibonacci(int ptr_matrix[][SIZE])
+{
+    void insert_spiral(int ptr_matrix[][SIZE], int number);
+    int a = 1, b = 1, aux = 0;
+
+    for (int i = 1; i < SIZE * SIZE; i++)
+    {
+        insert_spiral(ptr_matrix, a);
+        aux = b;
+        b += a;
+        a = aux;
+    }
+}
+
+void print_matrix(int ptr_matrix[][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            printf("%d ", ptr_matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void insert_spiral(int ptr_matrix[][SIZE], int number)
+{
+    /*Up Rigth Down Left
+    u = up
+    r = rigth
+    d = down
+    l = left
+    */
+    static char direction_flag = 'u';
+    static int counter = 1;
+    static int row = SIZE / 2, column = SIZE / 2;
+    static int check_final = 0, check_counter = 0;
+
+    if (ptr_matrix[SIZE / 2][SIZE / 2] == 0)
+        ptr_matrix[SIZE / 2][SIZE / 2] = number;
+    else
+    {
+        if (direction_flag == 'u')
+        {
+            row--;
+            ptr_matrix[row][column] = number;
+            check_counter++;
+
+            if (check_counter == counter)
+            {
+                direction_flag = 'r';
+                check_final++;
+            }
+
+            if (check_final == 2)
+            {
+                check_final = 0;
+                check_counter = 0;
+                counter++;
+            }
+        }
+        else if (direction_flag == 'r')
+        {
+            column++;
+            ptr_matrix[row][column] = number;
+            check_counter++;
+
+            if (check_counter == counter)
+            {
+                direction_flag = 'd';
+                check_final++;
+            }
+
+            if (check_final == 2)
+            {
+                check_final = 0;
+                check_counter = 0;
+                counter++;
+            }
+        }
+        else if (direction_flag == 'd')
+        {
+            row++;
+            ptr_matrix[row][column] = number;
+            check_counter++;
+
+            if (check_counter == counter)
+            {
+                direction_flag = 'l';
+                check_final++;
+            }
+
+            if (check_final == 2)
+            {
+                check_final = 0;
+                check_counter = 0;
+                counter++;
+            }
+        }
+        else if (direction_flag == 'l')
+        {
+            column--;
+            ptr_matrix[row][column] = number;
+            check_counter++;
+
+            if (check_counter == counter)
+            {
+                direction_flag = 'u';
+                check_final++;
+            }
+
+            //This should be just when direction_flag = l (left)
+            if (check_final == 2 && row == 0 && column == SIZE - 1)
+                check_final--;
+
+            else if (check_final == 2)
+            {
+                check_final = 0;
+                check_counter = 0;
+                counter++;
+            }
+        }
+    }
+}
