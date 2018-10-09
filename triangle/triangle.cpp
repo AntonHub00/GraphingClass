55d87afe42bc05991b6a0a3544a636b0818a8327
Antonio Ochoa <antonhub00@gmail.com>
2018-10-09 00:45:07 -0500

    Parameters as variables in triangle.cpp
    Each parameter is a variable so it makes it easy to change and
    test diferent values.

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -16,24 +16,25 @@ int main()
 {
     Mat canvas(SIZE, SIZE, CV_8UC3, Scalar(255, 255, 255));
 
-    int h = 0, v = 0;
+    int h = 0, v = 0, face = 100;
+    int init_x = SIZE / 2 - 2 * face, init_y = SIZE / 2 + face;
 
     int i;
     for(i = 4; i > 0; i--)
     {
-        line(canvas, Point(h + SIZE / 2, v + SIZE / 2), Point(h + SIZE / 2 + 50 * cos(0 * 0.01745333333), v + SIZE / 2 - 50 * sin(0 * 0.01745333333)), Scalar(0, 0, 0));
+        line(canvas, Point(h + init_x, v + init_y), Point(h + init_x + face * cos(0 * 0.01745333333), v + init_y - face * sin(0 * 0.01745333333)), Scalar(0, 0, 0));
         imshow("Draw", canvas);
         waitKey(100);
 
-        line(canvas, Point(h + SIZE / 2 + 50 * cos(0 * 0.01745333333), v + SIZE / 2 - 50 * sin(0 * 0.01745333333)), Point(h + SIZE / 2 + 50 * cos(60 * 0.01745333333), v + SIZE / 2 - 50 * sin(60 * 0.01745333333)), Scalar(0, 0, 0));
+        line(canvas, Point(h + init_x + face * cos(0 * 0.01745333333), v + init_y - face * sin(0 * 0.01745333333)), Point(h + init_x + face * cos(60 * 0.01745333333), v + init_y - face * sin(60 * 0.01745333333)), Scalar(0, 0, 0));
         imshow("Draw", canvas);
         waitKey(100);
 
-        line(canvas, Point(h + SIZE / 2 + 50 * cos(60 * 0.01745333333), v + SIZE / 2 - 50 * sin(60 * 0.01745333333)), Point(h + SIZE / 2, v + SIZE / 2),Scalar(0, 0, 0));
+        line(canvas, Point(h + init_x + face * cos(60 * 0.01745333333), v + init_y - face * sin(60 * 0.01745333333)), Point(h + init_x, v + init_y),Scalar(0, 0, 0));
         imshow("Draw", canvas);
         waitKey(100);
 
-        h += 50;
+        h += face;
     }
 
     imshow("Draw", canvas);
