ecc5db092a10f70d45ad53ac70182ccdf8527c16
Antonio Ochoa <antonhub00@gmail.com>
2018-10-12 02:34:40 -0500

    triangle.cpp finished and .figure1_circles.cpp.swp deleted
    This file is already finished and working properly.
    It paints the triangle pixel by pixel and continuously.
    .figure1_circles.cpp.swp is no longer needed.

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -16,55 +16,85 @@ int main()
 {
     Mat canvas(SIZE, SIZE, CV_8UC3, Scalar(255, 255, 255));
 
-    int h = 0, v = 0, face = 100;
-    int init_x = SIZE / 2 - 2 * face, init_y = SIZE / 2 + face;
+    int face, n, x1, y1,x, y, new_x, new_y, counter, counter_line;
+    n = 4;
+    face = 100;
+    float h = sqrt(pow(face, 2) - pow(face / 2, 2)), m;
+    counter = n * face;
+    counter_line = n;
+    new_x = (SIZE - counter) / 2;
+    new_y =  SIZE - ((SIZE - n * h) / 2);
 
-    int i, j, v_j = 4;
-    int new_h, new_v;
-    for(i = 4; i > 0; i--)
+    int i, j, k;
+    for(i = 0; i < n; i++)
     {
-        for(j = v_j; j > 0; j--)
-        {
-            line(canvas, Point(h + init_x, v + init_y), Point(h + init_x + face * cos(0 * 0.01745333333), v + init_y - face * sin(0 * 0.01745333333)), Scalar(0, 0, 0));
-            imshow("Draw", canvas);
-            waitKey(100);
+        x1 = new_x;
+        y1 = new_y;
 
-            h += face;
+        for(j = 0; j < counter; j++)
+        {
+            x1++;
+            circle(canvas, Point(x1, y1), 1, Scalar(0, 0, 0), 2);
+            imshow("Draw", canvas);
+            waitKey(1);
         }
 
-        h -= face;
-
-        for(j = v_j; j > 0; j--)
+        for(k = counter_line; k > 0; k--)
         {
-            line(canvas, Point(h + init_x + face * cos(0 * 0.01745333333), v + init_y - face * sin(0 * 0.01745333333)), Point(h + init_x + face * cos(60 * 0.01745333333), v + init_y - face * sin(60 * 0.01745333333)), Scalar(0, 0, 0));
-            imshow("Draw", canvas);
-            waitKey(100);
-
-            if(j != 1){
-                line(canvas, Point(h + init_x + face * cos(60 * 0.01745333333), v + init_y - face * sin(60 * 0.01745333333)), Point(h + init_x, v + init_y),Scalar(0, 0, 0));
+            m = h  / (face / 2);
+            x = x1;
+            y = y1;
+            for(j = 0; j < face / 2; j++)
+            {
+                x1--;
+                y1 = m * (x1 - x) + y;
+                circle(canvas, Point(x1, y1), 1, Scalar(0, 0, 0), 2);
                 imshow("Draw", canvas);
-                waitKey(100);
-                h -= face;
+                waitKey(1);
+            }
+
+            if(k != 1)
+            {
+                x = x1;
+                y = y1;
+                m = -h  / (face / 2);
+                for(j = 0; j < face / 2; j++)
+                {
+                    x1--;
+                    y1 = m * (x1 - x) + y;
+                    circle(canvas, Point(x1, y1), 1, Scalar(0, 0, 0), 2);
+                    imshow("Draw", canvas);
+                    waitKey(1);
+                }
             }
         }
 
-        h = 0;
-        new_h += face / 2;
-        h += new_h;
-        v -= sqrt(pow(face, 2) - pow(face / 2, 2));
-        v_j--;
-    }
+        if(i == n - 1)
+        {
+            m = -h  / (face / 2);
 
-    int f;
-    h -= face / 2;
-    v += sqrt(pow(face, 2) - pow(face / 2, 2));
-    for(f = 4; f > 0; f--)
-    {
-        line(canvas, Point(h + init_x + face * cos(60 * 0.01745333333), v + init_y - face * sin(60 * 0.01745333333)), Point(h + init_x, v + init_y),Scalar(0, 0, 0));
-        imshow("Draw", canvas);
-        waitKey(100);
-        h -= face / 2;
-        v += sqrt(pow(face, 2) - pow(face / 2, 2));
+            for(k = n; k > 0; k--)
+            {
+                x = x1;
+                y = y1;
+
+                for(j = 0; j < face / 2; j++)
+                {
+                    x1--;
+                    y1 = m * (x1 - x) + y;
+                    circle(canvas, Point(x1, y1), 1, Scalar(0, 0, 0), 2);
+                    imshow("Draw", canvas);
+                    waitKey(1);
+                }
+                x = x1;
+                y = y1;
+            }
+        }
+
+        new_x += face / 2 ;
+        new_y -= h;
+        counter -= face;
+        counter_line--;
     }
 
     imshow("Draw", canvas);
